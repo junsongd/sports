491bc91d6b0a4293bb6f4b65a4ce623b5c88df77
dongjunsong <57219@supinfo.com>
2017-06-27 22:02:07 +0200

    first commit

---
+++ ParisSport/www/settings.js
@@ -0,0 +1,18 @@
+//For rate and share app
+var android_packageName = "com.taydo.dailynews";
+var apple_id = "1166773798";
+//URL site wordpress
+var wordpress_url = "http://sports.dongjunsong.com";
+//Posts per page
+var wordpress_per_page = 10;
+//For push notification with OneSignal
+var onesignal_app_id = "038804a4-5b95-4fac-bb52-fe988fdf4cf5";
+var onesignal_project_number = "10866384352";
+//For RTL Language
+var rtl_language = false;
+//Open link in Detail
+var open_target_blank = true;
+//Date time format
+var date_format = "dd-MM-yyyy";
+//For Google Analytics
+var google_analytics = "UA-85795435-1";
